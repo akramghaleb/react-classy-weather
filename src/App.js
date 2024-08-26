15840c75988a75eee3f0fd4edf062478d53ab108
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-08-26 12:28:33 +0300

    Fetching Weather Data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from "react"
+import {convertToFlag} from "./starter"
 
 class App extends React.Component{
   constructor(props){
     super(props)
-    this.state = {location:"lisbon"}
+    this.state = {location:"lisbon" , isLoading:false,
+      displayLocation:'',weather:{}
+    }
     this.fetchWeather = this.fetchWeather.bind(this)
   }
 
-  fetchWeather(){
-
+  async fetchWeather(){
+    try {
+      this.setState({ isLoading:true})
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+  
+      if (!geoData.results) throw new Error("Location not found");
+  
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+      this.setState({displayLocation:`${name} ${convertToFlag(country_code)}`});
+  
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({weather:weatherData.daily});
+    } catch (err) {
+      console.error(err);
+    }finally{
+      this.setState({isLoading:false})
+    }
   }
 
   render(){
@@ -20,6 +48,7 @@ class App extends React.Component{
         value={this.state.location}
         onChange={(e)=>this.setState({location:e.target.value})}/>
       <button onClick={this.fetchWeather}>Get Weather</button>
+      {this.state.isLoading && <p className="loader">Loading...</p>}
     </div>
     </div>
   }
